db3a34eb3f02a842402e09070bbcc5d276b291b7
Pushan Mukhopadhyay <88872669+Godse-07@users.noreply.github.com>
2025-11-17 11:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (18.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,12 +1,34 @@
 class Solution {
     public int rob(int[] nums) {
         // return recursive(nums, 0, nums.length);
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return dp_sol(nums, dp, 0, n);
+
+        // int n = nums.length;
+        // int dp[] = new int[n];
+        // Arrays.fill(dp, -1);
+        // return dp_sol(nums, dp, 0, n);
+
+        return most_optimise(nums);
     }
 
+    public static int most_optimise(int nums[]){
+        int n = nums.length;
+        int dp[] = new int[n];
+        if(n == 1){
+            return nums[0];
+        }
+        if(n == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n; i++){
+            dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+
+
+    /*
     public static int dp_sol(int nums[], int dp[], int index, int n){
         if(index >= n){
             return 0;
@@ -19,6 +41,7 @@ class Solution {
         dp[index] = Math.max(pick, non_pick);
         return dp[index];
     }
+    */
 
     /*
     It will give TLE cause it is recursive
