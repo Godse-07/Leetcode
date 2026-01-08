d647f01baaadd937128cbc5f90bae95a502f4e42
Pushan Mukhopadhyay <88872669+Godse-07@users.noreply.github.com>
2026-01-09 00:41:13 +0530

    Time: 2 ms (38.06%) | Memory: 71.4 MB (39.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int firstMissingPositive(int[] arr) {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
         int i = 0;
-        while(i < arr.length){
-            int correctIndex = arr[i] - 1;
-            if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correctIndex]){
-                swap(arr, i, correctIndex);
+        while(i < n){
+            int corIndex = nums[i] - 1;
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[corIndex]){
+                swap(nums, i, corIndex);
             }else{
                 i++;
             }
         }
-
-        for(int j=0;j<arr.length;j++){
-            if(arr[j] != j+1){
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != j+1){
                 return j+1;
             }
         }
-
-        return arr.length + 1;
+        return nums.length + 1;
     }
 
     public static void swap(int arr[], int i, int j){
         if(i == j){
             return;
         }
+
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
         arr[i] = arr[i] ^ arr[j];
