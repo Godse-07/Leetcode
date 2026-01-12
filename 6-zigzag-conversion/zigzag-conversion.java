c69ce82c91aadc7c0ce271e1579aab14a79b6fa8
Pushan Mukhopadhyay <88872669+Godse-07@users.noreply.github.com>
2026-01-12 18:33:10 +0530

    Time: 5 ms (57.73%) | Memory: 46.3 MB (94.41%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,5 +1,31 @@
 class Solution {
     public String convert(String s, int nums) {
+
+        /*
+        
+    Example for 3 rows:
+
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+
+    Then read row by row → PAHNAPLSIIGYIR
+
+    How the process works:
+    - We create 'numRows' empty rows (buckets).
+    - We start from row 0 and move downward (row 0 → row 1 → row 2).
+    - When we reach the LAST row, we change direction and start moving UP.
+    - When we reach the FIRST row, we again move DOWN.
+    - This up & down movement creates the zigzag shape.
+    - Each character is placed in the current row as we move.
+    - In the end, we join all rows to form the final zigzag string.
+
+    This solution uses:
+    - An array of StringBuilders to store each row’s characters.
+    - A 'row' pointer to track the current row.
+    - A boolean 'goingDown' to switch direction when needed.
+
+         */
         if (nums == 1) return s;
 
         StringBuilder[] rows = new StringBuilder[nums];
