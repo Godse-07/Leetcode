0ac2f8a8958bd190a8d26f5b524b51c25aa259c0
Pushan Mukhopadhyay <88872669+Godse-07@users.noreply.github.com>
2026-01-21 21:59:26 +0530

    Time: 5 ms (43.46%) | Memory: 69.7 MB (32.52%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,18 +1,20 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        return sliding_window(nums, k);
-    }
-
-    static double sliding_window(int arr[], int window){
+        int i = 0;
+        int j = 0;
         double sum = 0;
-        for(int i=0; i<window; i++){
-            sum += arr[i];
+        double maxSum = -Double.MAX_VALUE;
+        while(j < nums.length) {
+            sum += nums[j];
+            if(j - i + 1 < k) {
+                j++;
+            } else if(j - i + 1 == k) {
+                maxSum = Math.max(maxSum, sum);
+                sum -= nums[i];
+                i++;
+                j++;
+            }
         }
-        double maxSum = sum;
-        for(int i=window; i<arr.length; i++){
-            sum += arr[i] - arr[i - window];
-            maxSum = Math.max(maxSum, sum);
-        }
-        return (double)(maxSum / window);
+        return maxSum / k;
     }
 }
